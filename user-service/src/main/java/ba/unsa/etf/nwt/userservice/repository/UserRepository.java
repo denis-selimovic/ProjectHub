8b1a6991eff6ceca838925927f78ca80c48f6722
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-03-13 16:15:31 +0100

    Extend PagingAndSortingRepository

--- user-service/src/main/java/ba/unsa/etf/nwt/userservice/repository/UserRepository.java
+++ user-service/src/main/java/ba/unsa/etf/nwt/userservice/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package ba.unsa.etf.nwt.userservice.repository;
 
 import ba.unsa.etf.nwt.userservice.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, UUID> {
+public interface UserRepository extends PagingAndSortingRepository<User, UUID> {
 }
