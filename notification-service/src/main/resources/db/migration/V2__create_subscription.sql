1665fc15647b29441aca243f56991bfb50250be1
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-03-13 10:20:04 +0100

    Basic database setup with migration scripts

---
+++ notification-service/src/main/resources/db/migration/V2__create_subscription.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS "subscriptions" (
+    id uuid NOT NULL DEFAULT gen_random_uuid(),
+    user_id uuid NOT NULL,
+    task_id uuid NOT NULL,
+    created_at timestamptz,
+    updated_at timestamptz,
+    CONSTRAINT pkey_subscriptions PRIMARY KEY (id),
+    CONSTRAINT unique_user_task UNIQUE (user_id, task_id)
+);
