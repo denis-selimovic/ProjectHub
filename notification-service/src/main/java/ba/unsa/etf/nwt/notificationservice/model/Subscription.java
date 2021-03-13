d4f4f4aa42589cb037bc90c3049f27833bbe1abd
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-03-13 10:27:32 +0100

    Added JPA models

---
+++ notification-service/src/main/java/ba/unsa/etf/nwt/notificationservice/model/Subscription.java
@@ -0,0 +1,32 @@
+package ba.unsa.etf.nwt.notificationservice.model;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "subscriptions")
+public class Subscription {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(name = "task_id", nullable = false)
+    private UUID taskId;
+}
