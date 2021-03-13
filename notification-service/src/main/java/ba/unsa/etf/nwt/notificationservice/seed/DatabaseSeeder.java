58d6305c320e14e450e8f1a42dd48dd94e4022a0
azigo12 <azigo1@etf.unsa.ba>
2021-03-13 23:58:48 +0100

    corrections

--- notification-service/src/main/java/ba/unsa/etf/nwt/notificationservice/seed/DatabaseSeeder.java
+++ notification-service/src/main/java/ba/unsa/etf/nwt/notificationservice/seed/DatabaseSeeder.java
@@ -8,8 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-
-import java.time.Instant;
 import java.util.UUID;
 
 @Component
@@ -29,58 +27,20 @@ public class DatabaseSeeder {
     }
 
     private void seedNotificationsTable() {
-            createNotification(
-                    "New project",
-                    "Added you to the team A",
-                    Instant.now(),
-                    null,
-                    false,
-                    UUID.randomUUID());
-
-            createNotification(
-                    "New task",
-                    "Added task UNIT TESTS to the project ProjectHub",
-                    Instant.now(),
-                    null,
-                    true,
-                    UUID.randomUUID());
-
-            createNotification(
-                    "New bug",
-                    "Added bug WRONG EMAIL to the project ProjectHub",
-                    Instant.now(),
-                    null,
-                    false,
-                    UUID.randomUUID());
-
-            createNotification(
-                    "New comment",
-                    "Commented on the task UNIT TESTS",
-                    Instant.now(),
-                    null,
-                    false,
-                    UUID.randomUUID());
-
-            createNotification(
-                    "New project",
-                    "Added you to the team Aplha",
-                    Instant.now(),
-                    null,
-                    true,
-                    UUID.randomUUID());
+        createNotification("New project", "Added you to the team A", false, UUID.randomUUID());
+        createNotification("New task", "Added task UNIT TESTS to the project ProjectHub", true, UUID.randomUUID());
+        createNotification("New bug", "Added bug WRONG EMAIL to the project ProjectHub", false, UUID.randomUUID());
+        createNotification("New comment", "Commented on the task UNIT TESTS", false, UUID.randomUUID());
+        createNotification("New project", "Added you to the team Aplha", true, UUID.randomUUID());
     }
 
     private void createNotification(final String title,
-                                            final String description,
-                                            Instant createdAt,
-                                            Instant updatedAt,
-                                            boolean read,
-                                            UUID userId) {
+                                    final String description,
+                                    boolean read,
+                                    UUID userId) {
         Notification notification = new Notification();
         notification.setTitle(title);
         notification.setDescription(description);
-        notification.setCreatedAt(createdAt);
-        notification.setUpdatedAt(updatedAt);
         notification.setRead(read);
         notification.setUserId(userId);
 
@@ -88,15 +48,13 @@ public class DatabaseSeeder {
     }
 
     private void seedSubscriptionsTable() {
-        createSubscription(Instant.now(), null, UUID.randomUUID(), UUID.randomUUID());
-        createSubscription(Instant.parse("2020-04-09T10:15:30.00Z"), Instant.now(), UUID.randomUUID(), UUID.randomUUID());
-        createSubscription(Instant.now(), null, UUID.randomUUID(), UUID.randomUUID());
+        createSubscription(UUID.randomUUID(), UUID.randomUUID());
+        createSubscription(UUID.randomUUID(), UUID.randomUUID());
+        createSubscription(UUID.randomUUID(), UUID.randomUUID());
     }
 
-    private void createSubscription(Instant createdAt, Instant updatedAt, UUID userId, UUID taskID) {
+    private void createSubscription(UUID userId, UUID taskID) {
         Subscription subscription = new Subscription();
-        subscription.setCreatedAt(createdAt);
-        subscription.setUpdatedAt(updatedAt);
         subscription.setUserId(userId);
         subscription.setTaskId(taskID);
 
