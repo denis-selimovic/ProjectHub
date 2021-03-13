46b1ed418bcc795ff9b972024bc0102f76ebccb5
lvrnjak1 <44277608+lvrnjak1@users.noreply.github.com>
2021-03-13 16:18:04 +0100

    Repository fix (#8)
    * Extend PagingAndSortingRepository

--- project-service/src/main/java/ba/unsa/etf/nwt/projectservice/projectservice/repository/ProjectCollaboratorRepository.java
+++ project-service/src/main/java/ba/unsa/etf/nwt/projectservice/projectservice/repository/ProjectCollaboratorRepository.java
@@ -1,11 +1,11 @@
 package ba.unsa.etf.nwt.projectservice.projectservice.repository;
 
 import ba.unsa.etf.nwt.projectservice.projectservice.model.ProjectCollaborator;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface ProjectCollaboratorRepository extends JpaRepository<ProjectCollaborator, UUID> {
+public interface ProjectCollaboratorRepository extends PagingAndSortingRepository<ProjectCollaborator, UUID> {
 }
--- user-service/src/main/java/ba/unsa/etf/nwt/userservice/repository/TokenRepository.java
+++ user-service/src/main/java/ba/unsa/etf/nwt/userservice/repository/TokenRepository.java
@@ -1,11 +1,11 @@
 package ba.unsa.etf.nwt.userservice.repository;
 
 import ba.unsa.etf.nwt.userservice.model.Token;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface TokenRepository extends CrudRepository<Token, UUID> {
+public interface TokenRepository extends PagingAndSortingRepository<Token, UUID> {
 }
