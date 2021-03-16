ace134697105277b40212b1546b778e9c8fc2246
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-03-16 11:02:25 +0100

    Added validations for Token model

--- user-service/src/main/java/ba/unsa/etf/nwt/userservice/model/Token.java
+++ user-service/src/main/java/ba/unsa/etf/nwt/userservice/model/Token.java
@@ -7,6 +7,10 @@ import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.time.Instant;
 import java.util.UUID;
 
@@ -25,6 +29,7 @@ public class Token {
 
     @Enumerated(EnumType.STRING)
     @Column(name = "type", nullable = false)
+    @NotNull(message = "Type can't be null")
     private TokenType type;
 
     @CreationTimestamp
@@ -36,15 +41,20 @@ public class Token {
     private Instant updatedAt;
 
     @Column(name = "duration", nullable = false)
+    @Positive(message = "Duration must be a positive integer")
     private Integer duration;
 
     @Column(name = "token", unique = true, nullable = false)
+    @NotBlank(message = "Token can't be blank")
+    @Size(min = 8, message = "Token must have at least 8 characters")
     private String token;
 
     @Column(name = "valid", nullable = false)
+    @NotNull(message = "Valid flag can't be null")
     private Boolean valid;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id", nullable = false)
+    @NotNull(message = "Token must have a user")
     private User user;
 }
