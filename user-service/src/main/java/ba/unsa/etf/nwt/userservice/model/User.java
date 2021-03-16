5a8d82c1cb0d9a7ecdc0deb7d3614b8d35ce798b
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-03-16 10:58:24 +0100

    Added user validations

--- user-service/src/main/java/ba/unsa/etf/nwt/userservice/model/User.java
+++ user-service/src/main/java/ba/unsa/etf/nwt/userservice/model/User.java
@@ -7,6 +7,10 @@ import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
@@ -23,15 +27,23 @@ public class User {
     private UUID id;
 
     @Column(name = "email", unique = true, nullable = false)
+    @NotBlank(message = "Email can't be blank")
+    @Email(message = "Invalid email format")
     private String email;
 
     @Column(name = "password", nullable = false)
+    @NotBlank(message = "Password can't be blank")
+    @Size(min = 8, message = "Password must contain at least eight characters")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=()!? \"]).{8,}",
+            message = "Password must contain at least one lowercase, one uppercase, one digit and one special character")
     private String password;
 
     @Column(name = "first_name", nullable = false)
+    @NotBlank(message = "First name can't be blank")
     private String firstName;
 
     @Column(name = "last_name", nullable = false)
+    @NotBlank(message = "Last name can't be blank")
     private String lastName;
 
     @CreationTimestamp
