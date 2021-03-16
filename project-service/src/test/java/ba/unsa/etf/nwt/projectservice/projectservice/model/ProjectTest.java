07898fe2da3293b2b421d0c6b445c8d9c6a563fd
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-03-16 10:28:29 +0100

    Tests for project model

--- project-service/src/test/java/ba/unsa/etf/nwt/projectservice/projectservice/model/ProjectTest.java
+++ project-service/src/test/java/ba/unsa/etf/nwt/projectservice/projectservice/model/ProjectTest.java
@@ -3,16 +3,21 @@ package ba.unsa.etf.nwt.projectservice.projectservice.model;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ActiveProfiles("test")
 public class ProjectTest {
     private static ValidatorFactory validatorFactory;
     private static Validator validator;
@@ -34,8 +39,9 @@ public class ProjectTest {
         project.setName("");
         project.setOwnerId(UUID.randomUUID());
 
-        Set<ConstraintViolation<Project>> violations = validator.validate(project);
+        List<ConstraintViolation<Project>> violations = new ArrayList<>(validator.validate(project));
         assertEquals(1, violations.size());
+        assertEquals("Project name can't be blank", violations.get(0).getMessage());
     }
 
     @Test
@@ -43,7 +49,32 @@ public class ProjectTest {
         Project project = new Project();
         project.setOwnerId(UUID.randomUUID());
 
-        Set<ConstraintViolation<Project>> violations = validator.validate(project);
+        List<ConstraintViolation<Project>> violations = new ArrayList<>(validator.validate(project));
         assertEquals(1, violations.size());
+        assertEquals("Project name can't be blank", violations.get(0).getMessage());
+    }
+
+    @Test
+    public void testNoOwner() {
+        Project project = new Project();
+        project.setName("Ime projekta");
+
+        List<ConstraintViolation<Project>> violations = new ArrayList<>(validator.validate(project));
+        assertEquals(1, violations.size());
+        assertEquals("Project must have an owner", violations.get(0).getMessage());
+    }
+
+    @Test
+    public void testMultipleViolations() {
+        Project project = new Project();
+        List<String> violations = validator
+                .validate(project)
+                .stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.toList());
+
+        assertEquals(2, violations.size());
+        assertTrue(violations.contains("Project must have an owner"));
+        assertTrue(violations.contains("Project name can't be blank"));
     }
 }
