1449fb9885324617cada3f20b292465be2b867bc
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-03-17 21:33:08 +0100

    Added addByEmail in UserRepository

--- user-service/src/main/java/ba/unsa/etf/nwt/userservice/repository/UserRepository.java
+++ user-service/src/main/java/ba/unsa/etf/nwt/userservice/repository/UserRepository.java
@@ -4,8 +4,10 @@ import ba.unsa.etf.nwt.userservice.model.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends PagingAndSortingRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
 }
