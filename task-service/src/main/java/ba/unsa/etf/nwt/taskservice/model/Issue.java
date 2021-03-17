3910686564240cb14c199c4ccf1576e3e3430383
ahajradino1 <ahajradino1@etf.unsa.ba>
2021-03-17 15:37:25 +0100

    added validation annotations

--- task-service/src/main/java/ba/unsa/etf/nwt/taskservice/model/Issue.java
+++ task-service/src/main/java/ba/unsa/etf/nwt/taskservice/model/Issue.java
@@ -7,6 +7,9 @@ import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.Instant;
 import java.util.UUID;
 
@@ -20,15 +23,21 @@ public class Issue {
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     private UUID id;
 
+    @NotBlank(message = "Issue name can't be blank")
+    @Size(max = 50, message = "Issue name can contain at most 50 characters")
     @Column(name = "name", nullable = false)
     private String name;
 
+    @NotBlank(message = "Issue description can't be blank")
+    @Size(max = 255, message = "Issue description can contain at most 255 characters")
     @Column(name = "description", nullable = false)
     private String description;
 
+    @NotNull(message = "Project id can't be null")
     @Column(name = "project_id", nullable = false)
     private UUID project_id;
 
+    @NotNull(message = "Priority id can't be null")
     @OneToOne
     @JoinColumn(name = "priority_id", nullable = false)
     private Priority priority;
--- task-service/src/main/java/ba/unsa/etf/nwt/taskservice/model/Task.java
+++ task-service/src/main/java/ba/unsa/etf/nwt/taskservice/model/Task.java
@@ -7,6 +7,9 @@ import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.Instant;
 import java.util.Set;
 import java.util.UUID;
@@ -21,26 +24,35 @@ public class Task {
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     private UUID id;
 
+    @NotBlank(message = "Task name can't be blank")
+    @Size(max = 50, message = "Task name can contain at most 50 characters")
     @Column(name = "name", nullable = false)
     private String name;
 
+    @NotBlank(message = "Task description can't be blank")
+    @Size(max = 255, message = "Task description can contain at most 255 characters")
     @Column(name = "description", nullable = false)
     private String description;
 
+    @NotNull(message = "User id can't be null")
     @Column(name = "user_id")
     private UUID user_id;
 
+    @NotNull(message = "Project id can't be null")
     @Column(name = "project_id", nullable = false)
     private UUID project_id;
 
+    @NotNull(message = "Priority id can't be null")
     @OneToOne
     @JoinColumn(name = "priority_id", nullable = false)
     private Priority priority;
 
+    @NotNull(message = "Status id can't be null")
     @OneToOne
     @JoinColumn(name = "status_id", nullable = false)
     private Status status;
 
+    @NotNull(message = "Type id can't be null")
     @OneToOne
     @JoinColumn(name = "type_id", nullable = false)
     private Type type;
