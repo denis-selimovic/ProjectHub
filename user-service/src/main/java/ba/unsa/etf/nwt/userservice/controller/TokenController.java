2a92b273cb8e825533fede196d89be503a75fb92
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-03-17 21:31:01 +0100

    Override Token controller for custom oauth endpoints

---
+++ user-service/src/main/java/ba/unsa/etf/nwt/userservice/controller/TokenController.java
@@ -0,0 +1,27 @@
+package ba.unsa.etf.nwt.userservice.controller;
+
+import ba.unsa.etf.nwt.userservice.request.token.GrantTokenRequest;
+import ba.unsa.etf.nwt.userservice.response.token.GrantTokenResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.endpoint.TokenEndpoint;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.security.Principal;
+
+@RequestMapping(value = "/oauth/token")
+public class TokenController extends TokenEndpoint {
+
+    @PostMapping
+    public ResponseEntity<GrantTokenResponse> postAccessToken(
+            Principal principal,
+            @Valid @RequestBody GrantTokenRequest request
+    ) throws HttpRequestMethodNotSupportedException {
+        OAuth2AccessToken oauthResponse = super.postAccessToken(principal, request.toForm()).getBody();
+        return ResponseEntity.ok(new GrantTokenResponse(oauthResponse));
+    }
+}
