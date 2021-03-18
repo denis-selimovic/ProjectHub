fbddc78455d623c9d5d6014f2805c1f26f549b8a
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-03-18 20:09:40 +0100

    Added DTO class for User model

---
+++ user-service/src/main/java/ba/unsa/etf/nwt/userservice/dto/UserDTO.java
@@ -0,0 +1,31 @@
+package ba.unsa.etf.nwt.userservice.dto;
+
+import ba.unsa.etf.nwt.userservice.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private UUID id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    public UserDTO(User user) {
+        id = user.getId();
+        email = user.getEmail();
+        firstName = user.getFirstName();
+        lastName = user.getLastName();
+        createdAt = user.getCreatedAt();
+        updatedAt = user.getUpdatedAt();
+    }
+}
