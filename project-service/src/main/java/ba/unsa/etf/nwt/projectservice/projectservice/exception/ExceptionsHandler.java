fd204580c6c3ff5909b2458bc132c9ab36de6fcf
Ajsa Hajradinovic <44201261+ahajradino1@users.noreply.github.com>
2021-03-18 22:50:01 +0100

    Feature/project ms error response (#22)
    * added response package
    
    * added custom exceptions
    
    * added custom exception and response handling classes

---
+++ project-service/src/main/java/ba/unsa/etf/nwt/projectservice/projectservice/exception/ExceptionsHandler.java
@@ -0,0 +1,60 @@
+package ba.unsa.etf.nwt.projectservice.projectservice.exception;
+
+import ba.unsa.etf.nwt.projectservice.projectservice.exception.base.BaseException;
+import ba.unsa.etf.nwt.projectservice.projectservice.response.base.ErrorResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class ExceptionsHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex,
+                                                                  final HttpHeaders headers,
+                                                                  final HttpStatus status,
+                                                                  final WebRequest request) {
+        return handleMethodArgumentNotValid(ex);
+    }
+
+    @ExceptionHandler(BaseException.class)
+    protected ResponseEntity<ErrorResponse> handleBadRequest(final BaseException ex) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.addError(ex.getMessage());
+        return new ResponseEntity<>(errorResponse, ex.getHttpStatus());
+    }
+
+    public static ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex){
+        Map<String, List<String>> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = toSnakeCase(((FieldError) error).getField());
+            String errorMessage = error.getDefaultMessage();
+            if(!errors.containsKey(fieldName)){
+                errors.put(fieldName, new ArrayList<>());
+            }
+            errors.get(fieldName).add(errorMessage);
+        });
+
+        ErrorResponse errorResponse = new ErrorResponse(errors);
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    private static String toSnakeCase(String s) {
+        String regex = "([a-z])([A-Z]+)";
+        String replacement = "$1_$2";
+        return s.replaceAll(regex, replacement).toLowerCase();
+    }
+}
---
+++ project-service/src/main/java/ba/unsa/etf/nwt/projectservice/projectservice/response/base/ErrorResponse.java
@@ -0,0 +1,21 @@
+package ba.unsa.etf.nwt.projectservice.projectservice.response.base;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private Map<String, List<String>> errors = new HashMap<>();
+
+    public void addError(final String message) {
+        errors.put("message", Collections.singletonList(message));
+    }
+}
