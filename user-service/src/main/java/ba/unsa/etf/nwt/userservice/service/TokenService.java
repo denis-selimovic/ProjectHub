4ca4ef1f1b2f47501638d3197e1c833ab017f92b
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-03-18 19:49:55 +0100

    Token and user generation logic in services

--- user-service/src/main/java/ba/unsa/etf/nwt/userservice/service/TokenService.java
+++ user-service/src/main/java/ba/unsa/etf/nwt/userservice/service/TokenService.java
@@ -1,6 +1,9 @@
 package ba.unsa.etf.nwt.userservice.service;
 
+import ba.unsa.etf.nwt.userservice.model.Token;
+import ba.unsa.etf.nwt.userservice.model.User;
 import ba.unsa.etf.nwt.userservice.repository.TokenRepository;
+import ba.unsa.etf.nwt.userservice.utility.TokenUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +11,21 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class TokenService {
 
+    private final TokenUtils tokenUtils;
     private final TokenRepository tokenRepository;
+
+    public Token generateActivationToken(User user) {
+        Token token = generateToken(user, Token.TokenType.ACTIVATE_ACCOUNT, tokenUtils.getActivationTokenDuration());
+        token = tokenRepository.save(token);
+        return token;
+    }
+
+    private Token generateToken(User user, Token.TokenType type, Integer duration) {
+        Token token = new Token();
+        token.setType(type);
+        token.setDuration(duration);
+        token.setUser(user);
+        token.setToken(tokenUtils.generateHashString());
+        return token;
+    }
 }
--- user-service/src/main/java/ba/unsa/etf/nwt/userservice/service/UserService.java
+++ user-service/src/main/java/ba/unsa/etf/nwt/userservice/service/UserService.java
@@ -1,7 +1,10 @@
 package ba.unsa.etf.nwt.userservice.service;
 
+import ba.unsa.etf.nwt.userservice.model.User;
 import ba.unsa.etf.nwt.userservice.repository.UserRepository;
+import ba.unsa.etf.nwt.userservice.request.user.CreateUserRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +12,19 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User create(CreateUserRequest request) {
+        User user = createUserFromRequest(request);
+        return userRepository.save(user);
+    }
+
+    private User createUserFromRequest(CreateUserRequest request) {
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        return user;
+    }
 }
