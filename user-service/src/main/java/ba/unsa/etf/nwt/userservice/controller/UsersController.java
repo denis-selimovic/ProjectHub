a18dad82c1b061b6f8a42e2a10668ebef05b0031
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-03-18 19:52:13 +0100

    Added response for creating users

---
+++ user-service/src/main/java/ba/unsa/etf/nwt/userservice/controller/UsersController.java
@@ -0,0 +1,33 @@
+package ba.unsa.etf.nwt.userservice.controller;
+
+import ba.unsa.etf.nwt.userservice.model.User;
+import ba.unsa.etf.nwt.userservice.request.user.CreateUserRequest;
+import ba.unsa.etf.nwt.userservice.response.user.CreateUserResponse;
+import ba.unsa.etf.nwt.userservice.service.TokenService;
+import ba.unsa.etf.nwt.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UsersController {
+
+    private final UserService userService;
+    private final TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity<CreateUserResponse> create(@RequestBody @Valid CreateUserRequest request) {
+        User user = userService.create(request);
+        tokenService.generateActivationToken(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new CreateUserResponse(user));
+    }
+}
---
+++ user-service/src/main/java/ba/unsa/etf/nwt/userservice/request/user/CreateUserRequest.java
@@ -0,0 +1,44 @@
+package ba.unsa.etf.nwt.userservice.request.user;
+
+import ba.unsa.etf.nwt.userservice.request.BaseRequest;
+import ba.unsa.etf.nwt.userservice.validation.annotation.FieldMatch;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@RequiredArgsConstructor
+@FieldMatch(first = "password", second = "confirmPassword", message = "Password doesn't much confirmation")
+public class CreateUserRequest extends BaseRequest {
+
+    @NotBlank(message = "Email can't be blank")
+    @Email(message = "Invalid email format")
+    @JsonProperty("email")
+    private final String email;
+
+    @Size(min = 8, message = "Password must contain at least eight characters")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=()!?.\"]).{8,}",
+            message = "Password must contain at least one lowercase, one uppercase, one digit and one special character")
+    @JsonProperty("password")
+    private final String password;
+
+    @JsonProperty("confirm_password")
+    private final String confirmPassword;
+
+    @Size(max = 32, message = "First name can't be longer than thirty two characters")
+    @NotBlank(message = "First name can't be blank")
+    @JsonProperty("first_name")
+    private final String firstName;
+
+    @Size(max = 32, message = "Last name can't be longer than thirty two characters")
+    @NotBlank(message = "Last name can't be blank")
+    @JsonProperty("last_name")
+    private final String lastName;
+}
