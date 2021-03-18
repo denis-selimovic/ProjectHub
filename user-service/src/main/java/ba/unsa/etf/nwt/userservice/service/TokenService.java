2a9cde8f8a0c4e5cb4bd7e85686e2eaf52a02212
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-03-18 21:33:50 +0100

    Added confirm email logic

--- user-service/src/main/java/ba/unsa/etf/nwt/userservice/service/TokenService.java
+++ user-service/src/main/java/ba/unsa/etf/nwt/userservice/service/TokenService.java
@@ -1,8 +1,12 @@
 package ba.unsa.etf.nwt.userservice.service;
 
+import ba.unsa.etf.nwt.userservice.exception.base.NotFoundException;
+import ba.unsa.etf.nwt.userservice.exception.base.UnprocessableEntityException;
 import ba.unsa.etf.nwt.userservice.model.Token;
 import ba.unsa.etf.nwt.userservice.model.User;
 import ba.unsa.etf.nwt.userservice.repository.TokenRepository;
+import ba.unsa.etf.nwt.userservice.repository.UserRepository;
+import ba.unsa.etf.nwt.userservice.request.user.ConfirmEmailRequest;
 import ba.unsa.etf.nwt.userservice.utility.TokenUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,14 +17,37 @@ public class TokenService {
 
     private final TokenUtils tokenUtils;
     private final TokenRepository tokenRepository;
+    private final UserRepository userRepository;
 
-    public Token generateActivationToken(User user) {
-        Token token = generateToken(user, Token.TokenType.ACTIVATE_ACCOUNT, tokenUtils.getActivationTokenDuration());
+    public Token generateToken(User user, Token.TokenType type) {
+        tokenRepository.invalidateTokenByType(user.getId(), type.name());
+        Token token = createToken(user, type);
         token = tokenRepository.save(token);
         return token;
     }
 
-    private Token generateToken(User user, Token.TokenType type, Integer duration) {
+    public void confirmActivation(ConfirmEmailRequest request) {
+        Token token = checkTokenValidity(request.getToken(), Token.TokenType.ACTIVATE_ACCOUNT);
+        User user = token.getUser();
+        if (user.getEnabled()) throw new UnprocessableEntityException("User already confirmed email");
+        activateUserAccount(user);
+    }
+
+    private Token checkTokenValidity(String tokenPayload, Token.TokenType type) {
+        Token token = tokenRepository.findTokenByTokenAndType(tokenPayload, type)
+                .orElseThrow(() -> new NotFoundException("Token doesn't exist"));
+        if (token.isExpired()) throw new UnprocessableEntityException("Expired token");
+        return token;
+    }
+
+    private void activateUserAccount(User user) {
+        user.setEnabled(true);
+        userRepository.save(user);
+    }
+
+    private Token createToken(User user, Token.TokenType type) {
+        Integer duration = (type == Token.TokenType.ACTIVATE_ACCOUNT)
+                ? tokenUtils.getActivationTokenDuration() : tokenUtils.getResetPasswordTokenDuration();
         Token token = new Token();
         token.setType(type);
         token.setDuration(duration);
