8137a24d6faa2dfd36a65ee6c898d065279f6937
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-03-18 13:42:47 +0100

    Custom error handler for validations

--- project-service/src/main/java/ba/unsa/etf/nwt/projectservice/projectservice/controller/DummyController.java
+++
@@ -1,2 +0,0 @@
-package ba.unsa.etf.nwt.projectservice.projectservice.controller;public class DummyController {
-}
--- project-service/src/main/java/ba/unsa/etf/nwt/projectservice/projectservice/exception/ExceptionsHandler.java
+++
@@ -1,2 +0,0 @@
-package ba.unsa.etf.nwt.projectservice.projectservice.exception;public class ExceptionsHandler {
-}
