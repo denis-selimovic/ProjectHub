72e5ad207e57a58cf3417a1ccb5ed56019f73d50
lvrnjak1 <44277608+lvrnjak1@users.noreply.github.com>
2021-03-19 23:44:48 +0100

    Feature/project ms resource server (#29)
    * Set up resource server
    
    * Set up resource server

--- project-service/src/main/java/ba/unsa/etf/nwt/projectservice/projectservice/ProjectServiceApplication.java
+++ project-service/src/main/java/ba/unsa/etf/nwt/projectservice/projectservice/ProjectServiceApplication.java
@@ -1,13 +1,24 @@
 package ba.unsa.etf.nwt.projectservice.projectservice;
 
+import ba.unsa.etf.nwt.projectservice.projectservice.security.ResourceOwner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/test")
 public class ProjectServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectServiceApplication.class, args);
 	}
 
+	@GetMapping
+	public ResponseEntity<?> hello(ResourceOwner resourceOwner) {
+		return ResponseEntity.ok(resourceOwner);
+	}
 }
