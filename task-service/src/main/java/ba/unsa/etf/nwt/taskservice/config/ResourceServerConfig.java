7ab8efd8ac4d0e53affbc5b0d322596126194dd0
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-03-19 22:01:02 +0100

    Setup security

--- task-service/src/main/java/ba/unsa/etf/nwt/taskservice/config/ResourceServerConfig.java
+++ task-service/src/main/java/ba/unsa/etf/nwt/taskservice/config/ResourceServerConfig.java
@@ -1,6 +1,9 @@
 package ba.unsa.etf.nwt.taskservice.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -11,14 +14,32 @@ import org.springframework.stereotype.Component;
 
 @EnableResourceServer
 @Component
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    private final String[] unprotectedEndpoints = {};
 
     private final JwtAccessTokenConverter jwtAccessTokenConverter;
 
     @Override
     public void configure(ResourceServerSecurityConfigurer configurer) {
         configurer.tokenStore(tokenStore());
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(unprotectedEndpoints)
+                .permitAll()
+                .anyRequest()
+                .access("#oauth2.user")
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     public TokenStore tokenStore() {
