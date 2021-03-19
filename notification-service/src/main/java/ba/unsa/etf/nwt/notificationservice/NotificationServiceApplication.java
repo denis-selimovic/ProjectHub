610121a2727399e8fd7763d823c21ec43225ec15
azigo12 <azigo1@etf.unsa.ba>
2021-03-19 23:11:31 +0100

    setup security

--- notification-service/src/main/java/ba/unsa/etf/nwt/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/ba/unsa/etf/nwt/notificationservice/NotificationServiceApplication.java
@@ -1,13 +1,24 @@
 package ba.unsa.etf.nwt.notificationservice;
 
+import ba.unsa.etf.nwt.notificationservice.security.ResourceOwner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/test")
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
     }
 
+    @GetMapping
+    public ResponseEntity<?> hello(ResourceOwner resourceOwner) {
+        return ResponseEntity.ok(resourceOwner);
+    }
 }
