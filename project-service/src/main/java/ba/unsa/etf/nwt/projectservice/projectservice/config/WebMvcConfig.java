9af10b73422373c742a197a4f50e84acb92513e7
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-03-19 22:44:25 +0100

    Set up resource server

--- project-service/src/main/java/ba/unsa/etf/nwt/projectservice/projectservice/config/WebMvcConfig.java
+++ project-service/src/main/java/ba/unsa/etf/nwt/projectservice/projectservice/config/WebMvcConfig.java
@@ -1,2 +1,19 @@
-package ba.unsa.etf.nwt.projectservice.projectservice.config;public class WebMvcConfig {
+package ba.unsa.etf.nwt.projectservice.projectservice.config;
+
+
+import ba.unsa.etf.nwt.projectservice.projectservice.security.ResourceOwnerInjector;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(@NonNull List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new ResourceOwnerInjector());
+    }
 }
--- project-service/src/main/java/ba/unsa/etf/nwt/projectservice/projectservice/security/ResourceOwner.java
+++ project-service/src/main/java/ba/unsa/etf/nwt/projectservice/projectservice/security/ResourceOwner.java
@@ -1,2 +1,27 @@
-package ba.unsa.etf.nwt.projectservice.projectservice.security;public class ResourceOwner {
+package ba.unsa.etf.nwt.projectservice.projectservice.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+public class ResourceOwner {
+
+    private UUID id;
+    private String email;
+    private String clientId;
+    private ArrayList<String> scopes;
+
+    public ResourceOwner(OAuth2AuthenticationDetails details) {
+        Map<String, ?> decoded = (Map<String, ?>) details.getDecodedDetails();
+        id = UUID.fromString((String) decoded.get("id"));
+        email = (String) decoded.get("email");
+        clientId = (String) decoded.get("client_id");
+        scopes = (ArrayList<String>) decoded.get("scope");
+    }
 }
