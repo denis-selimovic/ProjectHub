144507b1950c7cdfe4a3252cb870f4ab7e4f0c36
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-03-19 21:44:32 +0100

    Added token converters and decoders

---
+++ task-service/src/main/java/ba/unsa/etf/nwt/taskservice/security/CustomTokenConverter.java
@@ -0,0 +1,18 @@
+package ba.unsa.etf.nwt.taskservice.security;
+
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class CustomTokenConverter extends JwtAccessTokenConverter {
+
+    @Override
+    public OAuth2Authentication extractAuthentication(Map<String, ?> map) {
+        OAuth2Authentication authentication = super.extractAuthentication(map);
+        authentication.setDetails(map);
+        return authentication;
+    }
+}
