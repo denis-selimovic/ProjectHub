a2645e1e37053156fe67f5f9302390f0865be0f7
Ajsa Hajradinovic <44201261+ahajradino1@users.noreply.github.com>
2021-03-19 23:39:55 +0100

    setup security (#28)

--- email-service/src/main/java/ba/unsa/etf/nwt/emailservice/emailservice/EmailServiceApplication.java
+++ email-service/src/main/java/ba/unsa/etf/nwt/emailservice/emailservice/EmailServiceApplication.java
@@ -1,13 +1,25 @@
 package ba.unsa.etf.nwt.emailservice.emailservice;
 
+import ba.unsa.etf.nwt.emailservice.emailservice.security.ResourceOwner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/test")
 public class EmailServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailServiceApplication.class, args);
 	}
 
+	@GetMapping
+	public ResponseEntity<?> hello(ResourceOwner resourceOwner) {
+		return ResponseEntity.ok(resourceOwner);
+	}
 }
+
