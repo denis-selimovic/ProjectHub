5c57b43d874b61586fab506a0fe370c932140831
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-03-19 20:25:07 +0100

    Fix bugs

--- user-service/src/main/java/ba/unsa/etf/nwt/userservice/dto/UserDTO.java
+++ user-service/src/main/java/ba/unsa/etf/nwt/userservice/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.nwt.userservice.dto;
 
 import ba.unsa.etf.nwt.userservice.model.User;
+import ba.unsa.etf.nwt.userservice.response.interfaces.Resource;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,7 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDTO {
+public class UserDTO implements Resource {
 
     private UUID id;
     private String email;
