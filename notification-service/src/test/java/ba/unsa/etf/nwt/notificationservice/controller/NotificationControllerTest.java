226a72b2175c1ef85294ad655d8238fc2759385d
azigo12 <azigo1@etf.unsa.ba>
2021-03-21 20:52:08 +0100

    initial endpoints

--- notification-service/src/test/java/ba/unsa/etf/nwt/notificationservice/controller/NotificationControllerTest.java
+++ notification-service/src/test/java/ba/unsa/etf/nwt/notificationservice/controller/NotificationControllerTest.java
@@ -3,18 +3,12 @@ package ba.unsa.etf.nwt.notificationservice.controller;
 import ba.unsa.etf.nwt.notificationservice.repository.NotificationRepository;
 import ba.unsa.etf.nwt.notificationservice.service.NotificationService;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.hasItem;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -27,9 +21,10 @@ public class NotificationControllerTest {
     @Autowired
     private NotificationRepository notificationRepository;
 
+    private String token;
+
     @BeforeEach
     public void setUp() {
         notificationRepository.deleteAll();
     }
-
 }
--- notification-service/src/test/java/ba/unsa/etf/nwt/notificationservice/controller/SubscriptionControllerTest.java
+++ notification-service/src/test/java/ba/unsa/etf/nwt/notificationservice/controller/SubscriptionControllerTest.java
@@ -1,4 +1,31 @@
 package ba.unsa.etf.nwt.notificationservice.controller;
 
+
+import ba.unsa.etf.nwt.notificationservice.repository.SubscriptionRepository;
+import ba.unsa.etf.nwt.notificationservice.service.SubscriptionService;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class SubscriptionControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private SubscriptionService subscriptionService;
+    @Autowired
+    private SubscriptionRepository subscriptionRepository;
+
+    private String token;
+
+    @BeforeEach
+    public void setUp() {
+        subscriptionRepository.deleteAll();
+    }
 }
