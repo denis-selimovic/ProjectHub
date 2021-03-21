5b39ce2614e146ff4c9409acfc113a28ef45d957
ahajradino1 <ahajradino1@etf.unsa.ba>
2021-03-21 17:30:57 +0100

    added more tests for project service

--- project-service/src/test/java/ba/unsa/etf/nwt/projectservice/projectservice/controller/ProjectCollaboratorControllerTest.java
+++ project-service/src/test/java/ba/unsa/etf/nwt/projectservice/projectservice/controller/ProjectCollaboratorControllerTest.java
@@ -3,18 +3,23 @@ package ba.unsa.etf.nwt.projectservice.projectservice.controller;
 import ba.unsa.etf.nwt.projectservice.projectservice.config.token.ResourceOwnerInjector;
 import ba.unsa.etf.nwt.projectservice.projectservice.config.token.TokenGenerator;
 import ba.unsa.etf.nwt.projectservice.projectservice.model.Project;
+import ba.unsa.etf.nwt.projectservice.projectservice.model.ProjectCollaborator;
+import ba.unsa.etf.nwt.projectservice.projectservice.repository.ProjectCollaboratorRepository;
 import ba.unsa.etf.nwt.projectservice.projectservice.repository.ProjectRepository;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.*;
+import java.util.UUID;
 
+import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.hamcrest.Matchers.hasItem;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -25,19 +30,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-public class ProjectControllerTest {
+public class ProjectCollaboratorControllerTest {
     @Autowired
     private MockMvc mockMvc;
     @Autowired
-    private ProjectRepository projectRepository;
-    @Autowired
     private TokenGenerator tokenGenerator;
+    @Autowired
+    private ProjectCollaboratorRepository projectCollaboratorRepository;
+    @Autowired
+    private ProjectRepository projectRepository;
 
     private String token;
+
 
     @BeforeEach
     public void setUp() {
         projectRepository.deleteAll();
+        projectCollaboratorRepository.deleteAll();
         token = "Bearer " + tokenGenerator.createAccessToken(
                 ResourceOwnerInjector.clientId,
                 ResourceOwnerInjector.id,
@@ -46,88 +55,75 @@ public class ProjectControllerTest {
     }
 
     @Test
-    public void createProjectValidationBlank() throws Exception {
-        mockMvc.perform(post("/api/v1/projects/add")
+    public void addCollaboratorNotAuthor() throws Exception {
+        Project project = createProjectInDB(UUID.randomUUID());
+        mockMvc.perform(post("/api/v1/collaborators/add")
                 .header("Authorization", token)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("""
+                .content(String.format("""
                         {
-                            "name": ""
-                        }"""))
-                .andExpect(status().isUnprocessableEntity())
-                .andExpect(jsonPath("$.errors.name").value(hasItem("Project name can't be blank")));;
+                            "collaborator_id": "%s",
+                            "project_id": "%s"
+                        }""", UUID.randomUUID(), project.getId())))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.errors.message", hasItem("You don't have permission for this activity")));
     }
 
     @Test
-    public void createProjectValidationTooLong() throws Exception {
-        mockMvc.perform(post("/api/v1/projects/add")
+    public void addCollaboratorNonExistentProject() throws Exception {
+        mockMvc.perform(post("/api/v1/collaborators/add")
                 .header("Authorization", token)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("""
+                .content(String.format("""
                         {
-                            "name": "projekatprojekatprojekatprojekatprojekatprojekatprojekatprojekat"
-                        }"""))
-                .andExpect(status().isUnprocessableEntity())
-                .andExpect(jsonPath("$.errors.name").value(hasItem("Project name can contain at most 50 characters")));
-    }
-
-    @Test
-    public void addExistentProject() throws Exception {
-        createProjectInDB(ResourceOwnerInjector.id);
-
-        mockMvc.perform(post("/api/v1/projects/add")
-                .header("Authorization", token)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("""
-                        {
-                            "name": "Projekat 1"
-                        }"""))
+                            "collaborator_id": "%s",
+                            "project_id": "%s"
+                        }""", UUID.randomUUID(), UUID.randomUUID())))
                 .andExpect(status().isUnprocessableEntity())
                 .andExpect(jsonPath("$.errors.message", hasItem("Request body can not be processed")));
     }
 
     @Test
-    public void addProjectSuccess() throws Exception {
-        mockMvc.perform(post("/api/v1/projects/add")
+    public void addCollaboratorSuccess() throws Exception {
+        Project project = createProjectInDB(ResourceOwnerInjector.id);
+        mockMvc.perform(post("/api/v1/collaborators/add")
                 .header("Authorization", token)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("""
+                .content(String.format("""
                         {
-                            "name": "Project name"
-                        }"""))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.data.id").hasJsonPath())
-                .andExpect(jsonPath("$.data.name").hasJsonPath())
-                .andExpect(jsonPath("$.data.ownerId").hasJsonPath())
-                .andExpect(jsonPath("$.data.createdAt").hasJsonPath())
-                .andExpect(jsonPath("$.data.updatedAt").hasJsonPath());
+                            "collaborator_id": "%s",
+                            "project_id": "%s"
+                        }""", UUID.randomUUID(), project.getId())))
+                .andExpect(status().isCreated());
     }
 
     @Test
-    public void deleteNonexistentProject() throws Exception {
-        mockMvc.perform(delete("/api/v1/projects/" + UUID.randomUUID())
+    public void deleteNonexistentCollaborator() throws Exception {
+        mockMvc.perform(delete("/api/v1/collaborators/" + UUID.randomUUID())
                 .header("Authorization", token))
                 .andExpect(status().isUnprocessableEntity())
                 .andExpect(jsonPath("$.errors.message", hasItem("Request body can not be processed")));
     }
 
     @Test
-    public void deleteForeignProject() throws Exception {
+    public void deleteCollaboratorNotAuthor() throws Exception {
         Project project = createProjectInDB(UUID.randomUUID());
-
-        mockMvc.perform(delete("/api/v1/projects/" + project.getId())
+        ProjectCollaborator collaborator = createCollaboratorInDB(project);
+        mockMvc.perform(delete("/api/v1/collaborators/" + collaborator.getId())
                 .header("Authorization", token))
                 .andExpect(status().isForbidden())
                 .andExpect(jsonPath("$.errors.message", hasItem("You don't have permission for this activity")));
     }
 
     @Test
-    public void successfulDeletion() throws Exception {
+    public void deleteCollaboratorSuccess() throws Exception {
         Project project = createProjectInDB(ResourceOwnerInjector.id);
-        mockMvc.perform(delete("/api/v1/projects/" + project.getId())
+        ProjectCollaborator collaborator = createCollaboratorInDB(project);
+        mockMvc.perform(delete("/api/v1/collaborators/" + collaborator.getId())
                 .header("Authorization", token))
-                .andExpect(status().isOk());
-        assertEquals(0, projectRepository.count());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.message", Matchers.is("Project collaborator successfully deleted")));
+        assertEquals(0, projectCollaboratorRepository.count());
     }
 
     private Project createProjectInDB(UUID ownerID) {
@@ -136,4 +132,13 @@ public class ProjectControllerTest {
         project.setOwnerId(ownerID);
         return projectRepository.save(project);
     }
+
+    private ProjectCollaborator createCollaboratorInDB(Project project) {
+        ProjectCollaborator collaborator = new ProjectCollaborator();
+        collaborator.setProject(project);
+        collaborator.setCollaboratorId(UUID.randomUUID());
+        return projectCollaboratorRepository.save(collaborator);
+    }
+
+
 }
