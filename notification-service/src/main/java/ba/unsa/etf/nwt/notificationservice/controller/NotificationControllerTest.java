a22502fda686ba6dacb4e21e764330636b1a72b2
azigo12 <azigo1@etf.unsa.ba>
2021-03-21 18:34:47 +0100

    corrections

--- notification-service/src/main/java/ba/unsa/etf/nwt/notificationservice/controller/NotificationControllerTest.java
+++ notification-service/src/main/java/ba/unsa/etf/nwt/notificationservice/controller/NotificationControllerTest.java
@@ -1,6 +1,5 @@
 package ba.unsa.etf.nwt.notificationservice.controller;
 
-import ba.unsa.etf.nwt.notificationservice.config.TokenGenerator;
 import ba.unsa.etf.nwt.notificationservice.repository.NotificationRepository;
 import ba.unsa.etf.nwt.notificationservice.service.NotificationService;
 import org.junit.jupiter.api.BeforeEach;
@@ -27,24 +26,12 @@ public class NotificationControllerTest {
     private NotificationService notificationService;
     @Autowired
     private NotificationRepository notificationRepository;
-    @Autowired
-    private TokenGenerator tokenGenerator;
 
     @BeforeEach
     public void setUp() {
         notificationRepository.deleteAll();
     }
 
-    @Test
-    public void createTaskValidationsBlank() throws Exception {
-        mockMvc.perform(post("/api/v1/tasks")
-                .header("Authorization", "Bearer " + tokenGenerator.createAccessToken(
-                        "client",
-                        "priority_id": "d1c18630-ba07-470d-aa81-592c554dbe62",
-                        "type_id": "6acd9c39-a245-4985-aee5-2b217e159336"
-                        }"""))
-                .andExpect(status().isUnprocessableEntity())
-                .andExpect(jsonPath("$.errors.name", is("Task name can't be blank")))
-                .andExpect(jsonPath("$.errors.description", is("Task description can't be blank")));
-    }
 
+
+}
