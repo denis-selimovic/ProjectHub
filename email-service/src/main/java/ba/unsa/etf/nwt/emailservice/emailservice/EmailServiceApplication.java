a500fc6aa15b9e39ec6cc8e9538c0c10fb6ead92
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-03-23 00:56:46 +0100

    Mailer setup

--- email-service/src/main/java/ba/unsa/etf/nwt/emailservice/emailservice/EmailServiceApplication.java
+++ email-service/src/main/java/ba/unsa/etf/nwt/emailservice/emailservice/EmailServiceApplication.java
@@ -1,25 +1,38 @@
 package ba.unsa.etf.nwt.emailservice.emailservice;
 
-import ba.unsa.etf.nwt.emailservice.emailservice.security.ResourceOwner;
+import ba.unsa.etf.nwt.emailservice.emailservice.service.EmailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.mail.MessagingException;
+
 @SpringBootApplication
 @RestController
 @RequestMapping("/test")
+@RequiredArgsConstructor
 public class EmailServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailServiceApplication.class, args);
 	}
 
+	private final EmailService emailService;
+
 	@GetMapping
-	public ResponseEntity<?> hello(ResourceOwner resourceOwner) {
-		return ResponseEntity.ok(resourceOwner);
+	public ResponseEntity<?> hello() {
+		try {
+			emailService.sendEmail("TEST", "Denis", "dselimovic1@etf.unsa.ba");
+		} catch (MessagingException e) {
+			return ResponseEntity.ok("NOT OK");
+		}
+		return ResponseEntity.ok("OK");
 	}
 }
 
---
+++ email-service/src/main/java/ba/unsa/etf/nwt/emailservice/emailservice/service/EmailService.java
@@ -0,0 +1,43 @@
+package ba.unsa.etf.nwt.emailservice.emailservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    @Value("${mail.from}")
+    private String from;
+
+    private final TemplateEngine templateEngine;
+    private final JavaMailSender javaMailSender;
+
+    public void sendEmail(String subject, String name, String to) throws MessagingException {
+        String process = loadTemplate("emails/activate", name, UUID.randomUUID().toString());
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+        helper.setFrom(from);
+        helper.setSubject(subject);
+        helper.setTo(to);
+        helper.setText(process, true);
+        javaMailSender.send(message);
+    }
+
+    private String loadTemplate(String path, String name, String token) {
+        Context context = new Context();
+        context.setVariable("name", name);
+        context.setVariable("token", token);
+        return templateEngine.process(path, context);
+    }
+}
