14e2246b7ea16982c6a7af72f36977d4ee30d5c8
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-03-22 19:06:06 +0100

    Set active profiel

--- notification-service/src/test/java/ba/unsa/etf/nwt/notificationservice/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/ba/unsa/etf/nwt/notificationservice/NotificationServiceApplicationTests.java
@@ -2,8 +2,10 @@ package ba.unsa.etf.nwt.notificationservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NotificationServiceApplicationTests {
 
     @Test
