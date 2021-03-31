3057b7dc76a0c8865098726ffff93ab4882e75f3
azigo12 <azigo1@etf.unsa.ba>
2021-03-31 13:29:03 +0200

    added sql script for notificationUser

---
+++ notification-service/src/main/resources/db/migration/V3__create_notification_user.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE IF NOT EXISTS "notification_user" (
+    id uuid NOT NULL DEFAULT gen_random_uuid(),
+    notification_id uuid NOT NULL,
+    user_id uuid NOT NULL,
+    CONSTRAINT pkey_notification_user PRIMARY KEY (id),
+    CONSTRAINT unique_notification_user UNIQUE (notification_id, user_id),
+    CONSTRAINT fkey_notifuser_notifications FOREIGN KEY (notification_id) REFERENCES notifications(id) ON DELETE CASCADE
+);
