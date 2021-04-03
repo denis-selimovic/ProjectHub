65fd1f64e8ad7b62b365593fd810e85418b48713
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-04-03 14:26:38 +0200

    Expand ProjectDTO

--- task-service/src/main/java/ba/unsa/etf/nwt/taskservice/client/dto/ProjectDTO.java
+++ task-service/src/main/java/ba/unsa/etf/nwt/taskservice/client/dto/ProjectDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.Instant;
 import java.util.UUID;
 
 @Data
@@ -11,4 +12,8 @@ import java.util.UUID;
 @NoArgsConstructor
 public class ProjectDTO {
     private UUID projectId;
+    private String name;
+    private UUID ownerId;
+    private Instant createdAt;
+    private Instant updatedAt;
 }
