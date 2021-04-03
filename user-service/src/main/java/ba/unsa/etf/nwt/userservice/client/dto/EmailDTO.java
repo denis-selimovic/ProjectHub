b1dad18fd69835ea973bcf498f7ebaadfbdd9583
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-04-03 16:16:16 +0200

    Fixed users controller to use new email service

---
+++ user-service/src/main/java/ba/unsa/etf/nwt/userservice/client/dto/EmailDTO.java
@@ -0,0 +1,4 @@
+package ba.unsa.etf.nwt.userservice.client.dto;
+
+public class EmailDTO {
+}
---
+++ user-service/src/main/java/ba/unsa/etf/nwt/userservice/client/request/SendEmailRequest.java
@@ -0,0 +1,4 @@
+package ba.unsa.etf.nwt.userservice.client.request;
+
+public class SendEmailRequest {
+}
