797717965ae595be757e22acef4dd92118df33d8
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-04-06 01:00:40 +0200

    Add and fix tests

--- notification-service/src/test/java/ba/unsa/etf/nwt/notificationservice/controller/SubscriptionControllerTest.java
+++ notification-service/src/test/java/ba/unsa/etf/nwt/notificationservice/controller/SubscriptionControllerTest.java
@@ -1,16 +1,22 @@
 package ba.unsa.etf.nwt.notificationservice.controller;
 
 
+import ba.unsa.etf.nwt.notificationservice.client.dto.TaskDTO;
+import ba.unsa.etf.nwt.notificationservice.client.service.TaskService;
 import ba.unsa.etf.nwt.notificationservice.config.token.ResourceOwnerInjector;
 import ba.unsa.etf.nwt.notificationservice.config.token.TokenGenerator;
+import ba.unsa.etf.nwt.notificationservice.exception.base.ForbiddenException;
+import ba.unsa.etf.nwt.notificationservice.exception.base.NotFoundException;
 import ba.unsa.etf.nwt.notificationservice.exception.base.UnprocessableEntityException;
 import ba.unsa.etf.nwt.notificationservice.model.Subscription;
 import ba.unsa.etf.nwt.notificationservice.repository.SubscriptionRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
@@ -21,6 +27,7 @@ import java.util.UUID;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -37,7 +44,8 @@ public class SubscriptionControllerTest {
     private TokenGenerator tokenGenerator;
     @Autowired
     private SubscriptionRepository subscriptionRepository;
-
+    @MockBean
+    private TaskService taskService;
     private String token;
 
     @BeforeEach
@@ -84,14 +92,17 @@ public class SubscriptionControllerTest {
 
     @Test
     public void createSubscriptionSuccess() throws Exception {
-        String id = UUID.randomUUID().toString();
+        UUID id = UUID.randomUUID();
+        TaskDTO taskDTO = new TaskDTO();
+        taskDTO.setId(id);
+        Mockito.when(taskService.findTaskById(Mockito.any(), eq(id))).thenReturn(taskDTO);
         mockMvc.perform(post("/api/v1/subscriptions")
                 .header(HttpHeaders.AUTHORIZATION, token)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(String.format("""
                         {
                             "task_id": "%s"
-                        }""", id)))
+                        }""", id.toString())))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.data.id").hasJsonPath())
                 .andExpect(jsonPath("$.data.user_id").hasJsonPath())
@@ -99,7 +110,7 @@ public class SubscriptionControllerTest {
                 .andExpect(jsonPath("$.data.created_at").hasJsonPath())
                 .andExpect(jsonPath("$.data.updated_at").hasJsonPath())
                 .andExpect(jsonPath("$.data.user_id", is(ResourceOwnerInjector.id.toString())))
-                .andExpect(jsonPath("$.data.task_id", is(id)));
+                .andExpect(jsonPath("$.data.task_id", is(id.toString())));
     }
 
     @Test
@@ -115,13 +126,56 @@ public class SubscriptionControllerTest {
     public void deleteSubscriptionBadId() throws Exception {
         mockMvc.perform(delete(String.format("/api/v1/subscriptions/%s", UUID.randomUUID().toString()))
                 .header(HttpHeaders.AUTHORIZATION, token))
-                .andExpect(result -> assertTrue(result.getResolvedException() instanceof UnprocessableEntityException))
-                .andExpect(jsonPath("$.errors.message").value(hasItem("Request body can not be processed. This subscription doesn't exist")));
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.errors.message").value(hasItem("Subscription doesn't exist")));
+    }
+
+    @Test
+    public void deleteSubscriptionNotOwner() throws Exception {
+        Subscription subscription = createSubscriptionInDb(UUID.randomUUID());
+        mockMvc.perform(delete(String.format("/api/v1/subscriptions/%s", subscription.getId().toString()))
+                .header(HttpHeaders.AUTHORIZATION, token))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.errors.message").value(hasItem("Subscription doesn't exist")));
+    }
+
+    @Test
+    public void createSubscriptionNotFoundFromTaskMs() throws Exception {
+        UUID id = UUID.randomUUID();
+        Mockito.when(taskService.findTaskById(Mockito.any(), eq(id))).thenThrow(new NotFoundException("Not found"));
+        mockMvc.perform(post("/api/v1/subscriptions")
+                .header(HttpHeaders.AUTHORIZATION, token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(String.format("""
+                        {
+                            "task_id": "%s"
+                        }""", id.toString())))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.errors.message", hasItem("Not found")));
+    }
+
+    @Test
+    public void createSubscriptionForbiddenFromTaskMs() throws Exception {
+        UUID id = UUID.randomUUID();
+        Mockito.when(taskService.findTaskById(Mockito.any(), eq(id))).thenThrow(new ForbiddenException("Forbidden"));
+        mockMvc.perform(post("/api/v1/subscriptions")
+                .header(HttpHeaders.AUTHORIZATION, token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(String.format("""
+                        {
+                            "task_id": "%s"
+                        }""", id.toString())))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.errors.message", hasItem("Forbidden")));
     }
 
     private Subscription createSubscriptionInDb() {
+        return createSubscriptionInDb(ResourceOwnerInjector.id);
+    }
+
+    private Subscription createSubscriptionInDb(UUID userId) {
         Subscription subscription = new Subscription();
-        subscription.setUserId(ResourceOwnerInjector.id);
+        subscription.setUserId(userId);
         subscription.setTaskId(UUID.randomUUID());
         return subscriptionRepository.save(subscription);
     }
