696dd767f796126efb0827fe239d38874d9fbee2
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-04-06 00:37:28 +0200

    Implemented communication

--- email-service/src/main/java/ba/unsa/etf/nwt/emailservice/emailservice/client/dto/TaskDTO.java
+++ email-service/src/main/java/ba/unsa/etf/nwt/emailservice/emailservice/client/dto/TaskDTO.java
@@ -4,8 +4,22 @@ import ba.unsa.etf.nwt.emailservice.emailservice.response.interfaces.Resource;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.Instant;
+import java.util.UUID;
+
 
 @Data
 @AllArgsConstructor
 public class TaskDTO implements Resource {
+
+    private UUID id;
+    private String name;
+    private String description;
+    private UUID userId;
+    private UUID projectId;
+    private PriorityDTO priority;
+    private StatusDTO status;
+    private TypeDTO type;
+    private Instant createdAt;
+    private Instant updatedAt;
 }
