a755a42608d68c2ae1ed24d6df41cd8c06b4aa6a
azigo12 <44235619+azigo12@users.noreply.github.com>
2021-04-12 21:27:47 +0200

    setup logger for email ms (#74)

--- email-service/src/main/java/ba/unsa/etf/nwt/emailservice/emailservice/config/WebMvcConfig.java
+++ email-service/src/main/java/ba/unsa/etf/nwt/emailservice/emailservice/config/WebMvcConfig.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -13,9 +15,15 @@ import java.util.List;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final HandlerMethodArgumentResolver resourceOwnerInjector;
+    private final HandlerInterceptor systemEventInterceptor;
 
     @Override
     public void addArgumentResolvers(@NonNull List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(resourceOwnerInjector);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(systemEventInterceptor);
     }
 }
