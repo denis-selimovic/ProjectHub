628bab8aa221e4b2eba0dc1cb2b2f9e2ccf782ad
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-05-07 15:02:55 +0200

    Add validators and create custom password matching validator

--- frontend/src/app/components/forms/register-form/register-form.component.ts
+++ frontend/src/app/components/forms/register-form/register-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-form.component.scss']
 })
 export class RegisterFormComponent implements OnInit {
+  registerForm: FormGroup;
+  errorMessage: String
+  validPasswordPattern: RegExp
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.validPasswordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=()!?.\"]).{4,}/;
+    this.errorMessage = ""; //error message from server
+    this.registerForm = this.formBuilder.group({
+      firstName: new FormControl('', [Validators.required, Validators.maxLength(32)]),
+      lastName: new FormControl('', [Validators.required, Validators.maxLength(32)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('',[ Validators.required, Validators.pattern(this.validPasswordPattern)]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.pattern(this.validPasswordPattern)])
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void { }
 }
