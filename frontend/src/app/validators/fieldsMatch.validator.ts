f9d44517b85250c3040d80e88dede1b2c15bdb28
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-05-07 16:47:55 +0200

    Delete comments

--- frontend/src/app/validators/fieldsMatch.validator.ts
+++ frontend/src/app/validators/fieldsMatch.validator.ts
@@ -1,24 +1,3 @@
-// import { AbstractControl, FormGroup } from "@angular/forms";
-
-// export function FieldsMatch(controlName: string, matchingControlName: string) {
-//     return (control: AbstractControl) => {
-//         const target = control.get(controlName);
-//         const match = control.get(matchingControlName);
-
-//         if (match?.errors && !match.errors.mustMatch) {
-//             // return if another validator has already found an error on the matchingControl
-//             return;
-//         }
-
-//         // set error on matchingControl if validation fails
-//         if (target?.value !== match?.value) {
-//             target?.setErrors({ mustMatch: true });
-//         } else {
-//             target?.setErrors(null);
-//         }
-//     }
-// }
-
 import { FormGroup } from '@angular/forms';
     
 export function FieldsMatch(controlName: string, matchingControlName: string){
