a29e9cdab4d76a5b2cb8a701b0aa7b82a66860ad
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-05-08 01:04:49 +0200

    Added User Consumer for RabbitMQ

---
+++ user-service/src/main/java/ba/unsa/etf/nwt/userservice/messaging/consumers/UserConsumer.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.nwt.userservice.messaging.consumers;
+
+import ba.unsa.etf.nwt.userservice.dto.UserDTO;
+import ba.unsa.etf.nwt.userservice.messaging.Consumer;
+import ba.unsa.etf.nwt.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserConsumer implements Consumer<UserDTO> {
+
+    private final UserService userService;
+
+    @RabbitListener(queues = "email-service")
+    public void receive(UserDTO data) {
+        userService.delete(data.getId());
+    }
+}
