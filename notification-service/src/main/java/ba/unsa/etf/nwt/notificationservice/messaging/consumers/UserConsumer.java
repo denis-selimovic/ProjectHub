e553733d091dfd46f656ef615d29836ce89801de
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-05-08 01:57:35 +0200

    Added UserPubisher for RabbitMQ

--- notification-service/src/main/java/ba/unsa/etf/nwt/notificationservice/messaging/consumers/UserConsumer.java
+++ notification-service/src/main/java/ba/unsa/etf/nwt/notificationservice/messaging/consumers/UserConsumer.java
@@ -3,6 +3,7 @@ package ba.unsa.etf.nwt.notificationservice.messaging.consumers;
 import ba.unsa.etf.nwt.notificationservice.dto.UserDTO;
 import ba.unsa.etf.nwt.notificationservice.exception.base.UnprocessableEntityException;
 import ba.unsa.etf.nwt.notificationservice.messaging.Consumer;
+import ba.unsa.etf.nwt.notificationservice.messaging.publishers.UserPublisher;
 import ba.unsa.etf.nwt.notificationservice.service.SubscriptionConfigService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -13,12 +14,14 @@ import org.springframework.stereotype.Component;
 public class UserConsumer implements Consumer<UserDTO> {
 
     private final SubscriptionConfigService subscriptionConfigService;
+    private final UserPublisher userPublisher;
 
     @RabbitListener(queues = "user-service")
     public void receive(UserDTO data) {
         try {
             subscriptionConfigService.create(data);
         } catch (UnprocessableEntityException e) {
+            userPublisher.send(data);
         }
     }
 }
