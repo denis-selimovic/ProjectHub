eb72983304fd28191b327f3e34633515b4837985
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-05-08 23:00:24 +0200

    Setup Hibernate interceptor

---
+++ task-service/src/main/java/ba/unsa/etf/nwt/taskservice/utility/HibernateUtils.java
@@ -0,0 +1,21 @@
+package ba.unsa.etf.nwt.taskservice.utility;
+
+import ba.unsa.etf.nwt.taskservice.messaging.publishers.TaskNotificationPublisher;
+import ba.unsa.etf.nwt.taskservice.model.Task;
+import org.hibernate.EmptyInterceptor;
+import org.hibernate.type.Type;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+@Component
+public class HibernateUtils extends EmptyInterceptor {
+
+    @Override
+    public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
+        if(entity.getClass().equals(Task.class)) {
+            TaskNotificationPublisher publisher = ApplicationContextHolder.context.getBean(TaskNotificationPublisher.class);
+        }
+        return super.onFlushDirty(entity, id, currentState, previousState, propertyNames, types);
+    }
+}
