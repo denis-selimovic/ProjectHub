1a84faa57ab58a0af7dbd3c743905e8e88a49d80
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-05-08 23:08:07 +0200

    Created TaskNotificationDTO class

---
+++ task-service/src/main/java/ba/unsa/etf/nwt/taskservice/dto/TaskNotificationDTO.java
@@ -0,0 +1,33 @@
+package ba.unsa.etf.nwt.taskservice.dto;
+
+import ba.unsa.etf.nwt.taskservice.response.interfaces.Resource;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.codehaus.jackson.annotate.JsonProperty;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskNotificationDTO implements Resource {
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Change {
+        public String previous;
+        public String current;
+    }
+
+    private final Map<String, Change> changes = new HashMap<>();
+
+    @JsonProperty("updated_by")
+    private String updatedBy;
+
+    public void addChange(String property, String prev, String curr) {
+        changes.put(property, new Change(prev, curr));
+    }
+}
--- task-service/src/main/java/ba/unsa/etf/nwt/taskservice/utility/HibernateUtils.java
+++ task-service/src/main/java/ba/unsa/etf/nwt/taskservice/utility/HibernateUtils.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.nwt.taskservice.utility;
 
+import ba.unsa.etf.nwt.taskservice.dto.TaskNotificationDTO;
 import ba.unsa.etf.nwt.taskservice.messaging.publishers.TaskNotificationPublisher;
 import ba.unsa.etf.nwt.taskservice.model.Task;
 import org.hibernate.EmptyInterceptor;
@@ -15,6 +16,8 @@ public class HibernateUtils extends EmptyInterceptor {
     public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
         if(entity.getClass().equals(Task.class)) {
             TaskNotificationPublisher publisher = ApplicationContextHolder.context.getBean(TaskNotificationPublisher.class);
+            TaskNotificationDTO data = publisher.createNotification(previousState, currentState, propertyNames);
+            publisher.send(data);
         }
         return super.onFlushDirty(entity, id, currentState, previousState, propertyNames, types);
     }
