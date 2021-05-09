e34a016032ba523b15acfdaa20b244b2c1cc4f44
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-05-09 02:36:29 +0200

    Added Consumer & Publisher interfaces

---
+++ project-service/src/main/java/ba/unsa/etf/nwt/projectservice/projectservice/messaging/Consumer.java
@@ -0,0 +1,6 @@
+package ba.unsa.etf.nwt.projectservice.projectservice.messaging;
+
+import ba.unsa.etf.nwt.projectservice.projectservice.response.interfaces.Resource;
+
+public interface Consumer<T extends Resource> {
+}
