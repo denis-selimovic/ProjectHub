7e5c2784b05a5b58e406cb034a977e1a6e6222cc
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-05-09 15:26:51 +0200

    Log response on task service

--- task-service/src/main/java/ba/unsa/etf/nwt/taskservice/logging/SystemEventInterceptor.java
+++ task-service/src/main/java/ba/unsa/etf/nwt/taskservice/logging/SystemEventInterceptor.java
@@ -3,12 +3,17 @@ package ba.unsa.etf.nwt.taskservice.logging;
 import ba.unsa.etf.nwt.systemevents.grpc.SystemEventRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,8 +21,9 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 @Qualifier("systemEventInterceptor")
 @RequiredArgsConstructor
-public class SystemEventInterceptor implements HandlerInterceptor {
+public class SystemEventInterceptor implements HandlerInterceptor, ResponseBodyAdvice<Object> {
     private final SystemEventService systemEventService;
+    private Object cachedResponseBody;
 
     @Override
     public void afterCompletion(@NonNull HttpServletRequest request,
@@ -27,7 +33,23 @@ public class SystemEventInterceptor implements HandlerInterceptor {
         HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
         HandlerMethod method = (HandlerMethod) handler;
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        SystemEventRequest req = SystemEventFactory.createLogMessage(request, response, auth, method.getBeanType());
+        SystemEventRequest req = SystemEventFactory.createLogMessage(request, response, auth, method.getBeanType(), String.valueOf(cachedResponseBody));
         systemEventService.logSystemEvent(req);
+    }
+
+    @Override
+    public boolean supports(@NonNull MethodParameter methodParameter, @NonNull Class aClass) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object o,
+                                  @NonNull MethodParameter methodParameter,
+                                  @NonNull MediaType mediaType,
+                                  @NonNull Class aClass,
+                                  @NonNull ServerHttpRequest serverHttpRequest,
+                                  @NonNull ServerHttpResponse serverHttpResponse) {
+        cachedResponseBody = o;
+        return o;
     }
 }
