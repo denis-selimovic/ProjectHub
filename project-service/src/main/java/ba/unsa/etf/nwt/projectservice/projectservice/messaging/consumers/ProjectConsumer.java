5d2c75ba350af677953735d48f8187a57e7370c6
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-05-09 18:03:30 +0200

    Added consumer for deleting project

---
+++ project-service/src/main/java/ba/unsa/etf/nwt/projectservice/projectservice/messaging/consumers/ProjectConsumer.java
@@ -0,0 +1,24 @@
+package ba.unsa.etf.nwt.projectservice.projectservice.messaging.consumers;
+
+import ba.unsa.etf.nwt.projectservice.projectservice.dto.ProjectDTO;
+import ba.unsa.etf.nwt.projectservice.projectservice.messaging.Consumer;
+import ba.unsa.etf.nwt.projectservice.projectservice.model.Project;
+import ba.unsa.etf.nwt.projectservice.projectservice.repository.ProjectRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ProjectConsumer implements Consumer<ProjectDTO> {
+
+    private final ProjectRepository projectRepository;
+
+    @RabbitListener(queues = "task-service")
+    public void receive(ProjectDTO data) {
+        projectRepository.find(data.getId()).ifPresent(p -> {
+            p.setDeleted(false);
+            projectRepository.save(p);
+        });
+    }
+}
