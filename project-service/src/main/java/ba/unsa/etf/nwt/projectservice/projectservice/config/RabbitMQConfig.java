fa8c3fbb77fe587a6063904ce7783749e4945883
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-05-09 02:33:09 +0200

    Configured RabbitMQ for Project service

---
+++ project-service/src/main/java/ba/unsa/etf/nwt/projectservice/projectservice/config/RabbitMQConfig.java
@@ -0,0 +1,59 @@
+package ba.unsa.etf.nwt.projectservice.projectservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${amqp.queue}")
+    private String queueName;
+    @Value("${amqp.exchange}")
+    private String exchangeName;
+    @Value("${amqp.routing-key}")
+    private String routingKey;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName);
+    }
+
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange(exchangeName);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(routingKey);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        ObjectMapper mapper = new ObjectMapper()
+                .findAndRegisterModules()
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return new Jackson2JsonMessageConverter(mapper);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory factory, MessageConverter converter) {
+        RabbitTemplate template = new RabbitTemplate(factory);
+        template.setMessageConverter(converter);
+        return template;
+    }
+}
