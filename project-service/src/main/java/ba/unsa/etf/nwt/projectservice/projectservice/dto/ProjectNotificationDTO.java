00402fd257415bdc8e98176730fd469a0844b094
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-05-09 02:45:11 +0200

    Call publisher when adding collaborator

--- project-service/src/main/java/ba/unsa/etf/nwt/projectservice/projectservice/dto/ProjectNotificationDTO.java
+++ project-service/src/main/java/ba/unsa/etf/nwt/projectservice/projectservice/dto/ProjectNotificationDTO.java
@@ -1,10 +1,13 @@
 package ba.unsa.etf.nwt.projectservice.projectservice.dto;
 
 import ba.unsa.etf.nwt.projectservice.projectservice.model.Project;
+import ba.unsa.etf.nwt.projectservice.projectservice.model.ProjectCollaborator;
 import ba.unsa.etf.nwt.projectservice.projectservice.response.interfaces.Resource;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
@@ -13,9 +16,12 @@ public class ProjectNotificationDTO implements Resource {
 
     private String title;
     private String description;
+    private UUID userId;
 
-    public ProjectNotificationDTO(Project project) {
+    public ProjectNotificationDTO(ProjectCollaborator projectCollaborator) {
+        Project project = projectCollaborator.getProject();
         this.title = project.getName();
         this.description = String.format("You were added to %s", project.getName());
+        this.userId = projectCollaborator.getCollaboratorId();
     }
 }
