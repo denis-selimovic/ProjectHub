e302d221d04adf0195c16faf23b1d4d1dd623311
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-05-16 18:11:46 +0200

    Show user name when logged in

--- frontend/src/app/components/layout/header/header.component.ts
+++ frontend/src/app/components/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+  }
+
+  fullname(): string {
+    const currentUser = this.userService.getCurrentUser();
+    return `${currentUser.firstName} ${currentUser.lastName.charAt(0)}.`
   }
 
 }
