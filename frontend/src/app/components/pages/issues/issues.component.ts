4c1663f084fdcbe6d9ca99d41a3b357bb8476d92
azigo12 <azigo1@etf.unsa.ba>
2021-05-17 23:16:10 +0200

    added paginator

--- frontend/src/app/components/pages/issues/issues.component.ts
+++ frontend/src/app/components/pages/issues/issues.component.ts
@@ -41,6 +41,62 @@ export class IssuesComponent implements OnInit {
         description: "Fourth description",
         projectId: "e7165476-b729-11eb-8529-0242ac130003",
         priority: "LOW"
+      },
+      {
+        id: "e7165476-b729-11eb-8529-0242ac130003", 
+        name: "Fifth issue",
+        description: "Fifth description",
+        projectId: "e7165476-b729-11eb-8529-0242ac130003",
+        priority: "CRITICAL"
+      },
+      {
+        id: "e8265476-b729-11eb-8529-0242ac130003", 
+        name: "Sixth issue",
+        description: "Sixth description",
+        projectId: "e7165476-b729-11eb-8529-0242ac130003",
+        priority: "HIGH"
+      },
+      {
+        id: "e9365476-b729-11eb-8529-0242ac130003", 
+        name: "Seventh issue",
+        description: "Seventh description",
+        projectId: "e7165476-b729-11eb-8529-0242ac130003",
+        priority: "MEDIUM"
+      },
+      {
+        id: "e9365476-b729-11eb-8529-0242ac130003", 
+        name: "Eighth issue",
+        description: "Eighth description",
+        projectId: "e7165476-b729-11eb-8529-0242ac130003",
+        priority: "LOW"
+      },
+      {
+        id: "e7165476-b729-11eb-8529-0242ac130003", 
+        name: "Ninth issue",
+        description: "Ninth description",
+        projectId: "e7165476-b729-11eb-8529-0242ac130003",
+        priority: "CRITICAL"
+      },
+      {
+        id: "e8265476-b729-11eb-8529-0242ac130003", 
+        name: "Tenth issue",
+        description: "Tenth description",
+        projectId: "e7165476-b729-11eb-8529-0242ac130003",
+        priority: "HIGH"
+      },
+      {
+        id: "e9365476-b729-11eb-8529-0242ac130003", 
+        name: "Eleventh issue",
+        description: "Eleventh description",
+        projectId: "e7165476-b729-11eb-8529-0242ac130003",
+        priority: "MEDIUM"
+      },
+      {
+        id: "e9365476-b729-11eb-8529-0242ac130003", 
+        name: "Twelfth issue",
+        description: "Twelfth description",
+        projectId: "e7165476-b729-11eb-8529-0242ac130003",
+        priority: "LOW"
       }
     ]
   }
--- frontend/src/app/components/tables/issues-table/issues-table.component.ts
+++ frontend/src/app/components/tables/issues-table/issues-table.component.ts
@@ -1,5 +1,8 @@
-import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { Issue } from 'src/app/models/Issue';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-issues-table',
@@ -8,15 +11,25 @@ import { Issue } from 'src/app/models/Issue';
 })
 export class IssuesTableComponent implements AfterViewInit {
   @Input() issues: Array<Issue>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  dataSource: MatTableDataSource<Issue>;
+  obs: Observable<any>;
 
   constructor() {
-
    }
 
-   ngAfterViewInit(): void {
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
   }
 
   ngOnInit(): void {
+    this.dataSource = new MatTableDataSource<Issue>(this.issues);
+    this.obs = this.dataSource.connect();
   }
 
+  ngOnDestroy(): void {
+    if (this.dataSource) 
+      this.dataSource.disconnect(); 
+  }
+  
 }
