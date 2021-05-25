934da68c7b632db606d4eafa92bbdb2f0811f4dc
ahajradino1 <ahajradino1@etf.unsa.ba>
2021-05-25 13:06:36 +0200

    Added GET /types, /priorites, /statuses

--- task-service/src/main/java/ba/unsa/etf/nwt/taskservice/dto/PriorityDTO.java
+++ task-service/src/main/java/ba/unsa/etf/nwt/taskservice/dto/PriorityDTO.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.nwt.taskservice.dto;
 
 import ba.unsa.etf.nwt.taskservice.model.Priority;
+import ba.unsa.etf.nwt.taskservice.response.interfaces.Resource;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class PriorityDTO {
+public class PriorityDTO implements Resource {
     private UUID id;
     private String priority;
 
--- task-service/src/main/java/ba/unsa/etf/nwt/taskservice/service/PriorityService.java
+++ task-service/src/main/java/ba/unsa/etf/nwt/taskservice/service/PriorityService.java
@@ -1,9 +1,13 @@
 package ba.unsa.etf.nwt.taskservice.service;
 
+import ba.unsa.etf.nwt.taskservice.dto.PriorityDTO;
 import ba.unsa.etf.nwt.taskservice.exception.base.NotFoundException;
 import ba.unsa.etf.nwt.taskservice.model.Priority;
 import ba.unsa.etf.nwt.taskservice.repository.PriorityRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -17,5 +21,9 @@ public class PriorityService {
         return priorityRepository.findById(id).orElseThrow(() -> {
             throw new NotFoundException("Priority doesn't exist");
         });
+    }
+
+    public Page<Priority> findAll() {
+        return priorityRepository.findAll(Pageable.unpaged());
     }
 }
