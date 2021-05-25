75745e446858ecace2d2a2e68d57edf333ec0b9d
ahajradino1 <ahajradino1@etf.unsa.ba>
2021-05-25 05:08:10 +0200

    Added messages about action results

--- frontend/src/app/components/forms/new-collaborator-form/new-collaborator-form.component.ts
+++ frontend/src/app/components/forms/new-collaborator-form/new-collaborator-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { User, UserService } from 'src/app/services/user/user.service';
+import { ActionResultComponent } from '../../dialogs/action-result/action-result.component';
 
 @Component({
   selector: 'app-new-collaborator-form',
@@ -12,7 +14,7 @@ export class NewCollaboratorFormComponent implements OnInit {
   errorMessage: String
   @Output() onAddCollaborator = new EventEmitter<User>()
 
-  constructor(private formBuilder: FormBuilder, private userService: UserService) { 
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private dialog: MatDialog) { 
     this.newCollaboratorForm = this.formBuilder.group( {
       email: new FormControl('', [Validators.email, Validators.required])
     })
@@ -25,7 +27,12 @@ export class NewCollaboratorFormComponent implements OnInit {
   onSubmit(): void {
     this.userService.getUserByEmail(this.newCollaboratorForm.value.email, 
       (data: any) => this.onAddCollaborator.emit(data),
-      (err: any) => {console.log(err.error.errors.message)});
+      (err: any) => {
+        const dialogRef = this.dialog.open(ActionResultComponent);
+        const instance = dialogRef.componentInstance;
+        instance.message = err.error.errors.message;
+        instance.title = "Error!";
+      });
     this.newCollaboratorForm.reset();
   }
 
