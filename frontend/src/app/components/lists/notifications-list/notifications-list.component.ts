ed072ae548c148a033f133ed2a1d32bb46c2ccbf
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-05-26 00:10:32 +0200

    Style action button

--- frontend/src/app/components/lists/notifications-list/notifications-list.component.ts
+++ frontend/src/app/components/lists/notifications-list/notifications-list.component.ts
@@ -11,6 +11,9 @@ export class NotificationsListComponent implements OnInit {
   notifications: Array<Notification> = [
       {id: "0", title: "Notification 0", description: "This is a notification 0", dateTime: new Date(), read: false},
       {id: "1", title: "Notification 1", description: "This is a notification 1", dateTime: new Date(), read: true},
+      {id: "2", title: "Notification 2", description: "This is a notification 2", dateTime: new Date(), read: true},
+      {id: "2", title: "Notification 2", description: "This is a notification 2", dateTime: new Date(), read: false},
+      {id: "2", title: "Notification 2", description: "This is a notification 2", dateTime: new Date(), read: false},
       {id: "2", title: "Notification 2", description: "This is a notification 2", dateTime: new Date(), read: true}
   ];
 
