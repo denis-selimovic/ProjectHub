e482529b4fc9782376654072ea8fa9a4fcf04afd
Denis Selimovic <dselimovic1@etf.unsa.ba>
2021-05-30 18:47:33 +0200

    Changed navbar position

--- frontend/src/app/components/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/components/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user/user.service';
+import { TokenService } from 'src/app/services/token/token.service';
+import { PusherService } from 'src/app/services/pusher/pusher.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,8 +9,23 @@ import { UserService } from 'src/app/services/user/user.service';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-  constructor() {}
+
+  currentUser: any = null;
+  token: any = null;
+  channel: any = null;
+
+  constructor(private tokenService: TokenService, private userService: UserService,
+              private pusher: PusherService) {
+    this.currentUser = this.userService.getCurrentUser();
+    this.token = this.tokenService.getAccessToken();
+  }
 
   ngOnInit(): void {
+    if (!this.currentUser || !this.token) { return; }
+    if (this.channel) { return; }
+    this.channel = this.pusher.subscribe(this.token, `private-${this.currentUser.id}`);
+    this.channel.bind('notification', (data: any) => {
+      console.log(data);
+    });
   }
 }
