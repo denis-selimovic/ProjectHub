5c0d77d34fa6b686fdb6871e0de0dc2fa8193d5c
ahajradino1 <ahajradino1@etf.unsa.ba>
2021-05-30 20:36:31 +0200

    Added tests for new project form

--- frontend/src/app/components/forms/new-project-form/new-project-form.component.spec.ts
+++ frontend/src/app/components/forms/new-project-form/new-project-form.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormGroup } from '@angular/forms';
 
 import { CommonTestingModule } from 'src/app/common-testing.module';
 
@@ -7,16 +8,37 @@ import { NewProjectFormComponent } from './new-project-form.component';
 describe('NewProjectFormComponent', () => {
   let component: NewProjectFormComponent;
   let fixture: ComponentFixture<NewProjectFormComponent>;
+  let form: FormGroup;
 
   CommonTestingModule.setUpTestBed(NewProjectFormComponent);
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NewProjectFormComponent);
     component = fixture.componentInstance;
+    form = component.newProjectForm;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should test for form validity when name is empty', () => {
+    const nameInput = form.controls.name;
+    expect(nameInput.errors.required).toBeTruthy();    
+  });
+
+  it('should test for form validity when name is too long', () => {
+    const nameInput = form.controls.name;
+    nameInput.setValue('a'.repeat(51));
+    expect(form.valid).toBeFalsy();
+  });
+
+  it('should test for form validity when name is valid', () => {
+    const nameInput = form.controls.name;
+    nameInput.setValue('Project 1');
+    expect(form.valid).toBeTruthy();
+  });
+
+
 });
