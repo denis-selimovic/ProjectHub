1a97b1e34f90f8f0f0ccc8f8aab161a94b85a7c1
ahajradino1 <ahajradino1@etf.unsa.ba>
2021-05-31 18:08:57 +0200

    Started writing tests for collaborators table

--- frontend/src/app/components/tables/collaborators-table/collaborators-table.component.spec.ts
+++ frontend/src/app/components/tables/collaborators-table/collaborators-table.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatTableDataSource } from '@angular/material/table';
 
 import { CommonTestingModule } from 'src/app/common-testing.module';
 
@@ -44,4 +45,11 @@ describe('CollaboratorsTableComponent', () => {
       expect(CommonTestingModule.getCells(row).length).toBe(component.displayedColumns.length);
     });
   });
+
+  it('should be able to show a message when no data is being displayed', () => { 
+    component.dataSource = new MatTableDataSource<any>([]);
+    fixture.detectChanges();
+    const noDataField = fixture.debugElement.nativeElement.querySelector('.no-records');
+    expect(noDataField).toBeTruthy();
+  })
 });
