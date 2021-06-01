e0e9007ce1641b205ceaa4052c817109a562eaf8
ahajradino1 <ahajradino1@etf.unsa.ba>
2021-06-01 02:34:16 +0200

    Fixed test configuration for services

--- frontend/src/app/services/errors/error-interceptor.service.spec.ts
+++ frontend/src/app/services/errors/error-interceptor.service.spec.ts
@@ -2,13 +2,24 @@ import { TestBed } from '@angular/core/testing';
 
 import { ErrorInterceptorService } from './error-interceptor.service';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 describe('ErrorInterceptorService', () => {
   let service: ErrorInterceptorService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ HttpClientModule ]
+      imports: [
+        HttpClientTestingModule,
+        AppRoutingModule,
+        HttpClientModule,
+      ],
+      providers: [
+        HttpClientModule
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     });
     service = TestBed.inject(ErrorInterceptorService);
   });
--- frontend/src/app/services/notification/notification.service.spec.ts
+++ frontend/src/app/services/notification/notification.service.spec.ts
@@ -1,14 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-
-import { CommonTestingModule } from 'src/app/common-testing.module';
-
+import { AppRoutingModule } from 'src/app/app-routing.module';
 import { NotificationService } from './notification.service';
 
 describe('NotificationService', () => {
   let service: NotificationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        AppRoutingModule,
+        HttpClientModule,
+      ],
+      providers: [
+        HttpClientModule
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    });
     service = TestBed.inject(NotificationService);
   });
 
