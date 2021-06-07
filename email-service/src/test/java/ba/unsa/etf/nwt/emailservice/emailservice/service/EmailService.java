3eaddb8c1b0003cec2193698704d48fc291adbca
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-06-07 21:37:47 +0200

    Create email service mock

---
+++ email-service/src/test/java/ba/unsa/etf/nwt/emailservice/emailservice/service/EmailService.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.nwt.emailservice.emailservice.mock;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("test")
+public class EmailService {
+}
